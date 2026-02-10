01dd1eb7e3c6bb334393e7588f1b76b9bcaf93ee
sabeeh <you@example.com>
2026-02-10 22:15:37 +0500

    test

---
+++ js/active.js
@@ -0,0 +1,344 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+
+    if ($.fn.classyNav) {
+        $('#plantpalaceNav').classyNav();
+    }
+
+    $('#searchIcon').on('click', function () {
+        $('.search-form').toggleClass('active');
+    });
+    $('.closeIcon').on('click', function () {
+        $('.search-form').removeClass('active');
+    });
+
+    if ($.fn.owlCarousel) {
+        var welcomeSlide = $('.hero-post-slides');
+        var testiSlides = $('.testimonials-slides');
+        var categorySlides = $('.category-slides');
+
+        welcomeSlide.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: false,
+            dots: false,
+            autoplay: true,
+            center: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1000
+        });
+
+        testiSlides.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 700,
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut'
+        });
+
+        categorySlides.owlCarousel({
+            items: 2,
+            margin: 30,
+            loop: true,
+            nav: true,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 5000,
+            smartSpeed: 700,
+            center: true
+        });
+    }
+
+    if ($.fn.imagesLoaded) {
+        $('.plantpalace-category').imagesLoaded(function () {
+            $('.category-filter').on('click', 'button', function () {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            var $grid = $('.plantpalace-category').isotope({
+                itemSelector: '.single_category_item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.single_category_item'
+                }
+            });
+        });
+    }
+
+
+
+    if ($.fn.barfiller) {
+        $('#bar1').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+        $('#bar2').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+        $('#bar3').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+        $('#bar4').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+    }
+
+    if ($.fn.scrollUp) {
+        browserWindow.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-angle-up"></i>'
+        });
+    }
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    if ($.fn.sticky) {
+        $(".plantpalace-main-menu").sticky({
+            topSpacing: 0
+        });
+    }
+
+
+    if (browserWindow.width() > 767) {
+        new WOW().init();
+    }
+
+    function searchProducts() {
+        const searchInput = document.getElementById('searchInput').value.toLowerCase();
+        
+        productCards.forEach(card => {
+            const productName = card.querySelector('.product-info p').textContent.toLowerCase();
+            
+            if (productName.includes(searchInput)) {
+            } else {
+                card.style.display = 'none'; 
+            }
+        });
+    }
+
+
+
+})(jQuery);
+
+
+var shoppingCart = (function () {
+
+    cart = [];
+
+    function Item(name, price, count) {
+      this.name = name;
+      this.price = price;
+      this.count = count;
+    }
+
+    function saveCart() {
+      localStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+
+    function loadCart() {
+      cart = JSON.parse(localStorage.getItem('shoppingCart'));
+    }
+    if (localStorage.getItem("shoppingCart") != null) {
+      loadCart();
+    }
+
+
+    var obj = {};
+
+    obj.addItemToCart = function (name, price, count) {
+      for (var item in cart) {
+        if (cart[item].name === name) {
+          cart[item].count++;
+          saveCart();
+          return;
+        }
+      }
+      var item = new Item(name, price, count);
+      cart.push(item);
+      saveCart();
+    }
+    obj.setCountForItem = function (name, count) {
+      for (var i in cart) {
+        if (cart[i].name === name) {
+          cart[i].count = count;
+          break;
+        }
+      }
+    };
+    obj.removeItemFromCart = function (name) {
+      for (var item in cart) {
+        if (cart[item].name === name) {
+          cart[item].count--;
+          if (cart[item].count === 0) {
+            cart.splice(item, 1);
+          }
+          break;
+        }
+      }
+      saveCart();
+    }
+    obj.removeItemFromCartAll = function (name) {
+      for (var item in cart) {
+        if (cart[item].name === name) {
+          cart.splice(item, 1);
+          break;
+        }
+      }
+      saveCart();
+    }
+
+    obj.clearCart = function () {
+      cart = [];
+      saveCart();
+    }
+
+    obj.totalCount = function () {
+      var totalCount = 0;
+      for (var item in cart) {
+        totalCount += cart[item].count;
+      }
+      return totalCount;
+    }
+
+    obj.totalCart = function () {
+      var totalCart = 0;
+      for (var item in cart) {
+        totalCart += cart[item].price * cart[item].count;
+      }
+      return Number(totalCart.toFixed(2));
+    }
+
+    obj.listCart = function () {
+      var cartCopy = [];
+      for (i in cart) {
+        item = cart[i];
+        itemCopy = {};
+        for (p in item) {
+          itemCopy[p] = item[p];
+        }
+        itemCopy.total = Number(item.price * item.count).toFixed(2);
+        cartCopy.push(itemCopy)
+      }
+      return cartCopy;
+    }
+    return obj;
+  })();
+
+
+  // Add-to-cart buttons only (avoid binding to every ".default-btn" on the site)
+  $('.add-to-cart').click(function (event) {
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+
+    if (!name || isNaN(price)) {
+      return;
+    }
+
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+
+    // Requirement: clicking Add should open checkout page
+    window.location.href = 'checkout.html';
+  });
+
+  $('.clear-cart').click(function () {
+    shoppingCart.clearCart();
+    displayCart();
+  });
+
+
+  function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = "";
+    for (var i in cartArray) {
+      output += "<tr>"
+        + "<td>" + cartArray[i].name + "</td>"
+        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td><div class='input-group'>"
+        + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+        + "</div></td>"
+        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + " = "
+        + "<td>" + cartArray[i].total + "</td>"
+        + "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+  }
+
+
+  $('.show-cart').on("click", ".delete-item", function (event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+  })
+
+  $('.show-cart').on("change", ".item-count", function (event) {
+    var name = $(this).data('name');
+    var count = Number($(this).val());
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+  });
+  displayCart();
+
+$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+$('.tab ul.tabs li a').on('click', function (g) {
+    var tab = $(this).closest('.tab'), 
+    index = $(this).closest('li').index();
+    tab.find('ul.tabs > li').removeClass('current');
+    $(this).closest('li').addClass('current');
+    tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+    tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+    g.preventDefault();
+});
+
+$('#search_field').on('keyup', function() {
+  var value = $(this).val();
+  var patt = new RegExp(value, "i");
+
+  $('.tab_content').find('.col-lg-3').each(function() {
+    var $table = $(this);
+    
+    if (!($table.find('.featured-item').text().search(patt) >= 0)) {
+      $table.not('.featured-item').hide();
+    }
+    if (($table.find('.col-lg-3').text().search(patt) >= 0)) {
+      $(this).show();
+      document.getElementById('not_found').style.display = 'none';
+    } else {
+      document.getElementById("not_found").innerHTML = " Product not found..";
+      document.getElementById('not_found').style.display = 'block';
+    }
+    
+  });
+  
+});
